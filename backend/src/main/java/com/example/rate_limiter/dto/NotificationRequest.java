89f8b486bbf9b597d92f7cd97d95833b94f895f8
felixdusengimana <phelixdusengimana@gmail.com>
2026-02-10 17:14:12 +0200

    refactor: replace @Value with @Data, @NoArgsConstructor, and @AllArgsConstructor in NotificationRequest

--- backend/src/main/java/com/example/rate_limiter/dto/NotificationRequest.java
+++ backend/src/main/java/com/example/rate_limiter/dto/NotificationRequest.java
@@ -1,9 +1,13 @@
 package com.example.rate_limiter.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.Value;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Value
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NotificationRequest {
     @NotBlank
     String recipient;

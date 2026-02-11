5099e45ef393dcbb18ef85f8b8d5b18aa3d97f9e
felixdusengimana <phelixdusengimana@gmail.com>
2026-02-11 15:54:06 +0200

    feat: add Redis configuration with Jedis connection factory and template

---
+++ backend/src/main/java/com/example/rate_limiter/config/RedisConfig.java
@@ -0,0 +1,26 @@
+package com.example.rate_limiter.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory()
+    {
+        JedisConnectionFactory jedisConnectionFactory=new JedisConnectionFactory();
+
+        return jedisConnectionFactory;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate()
+    {
+        RedisTemplate<String, Object> redisTemplate=new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        return redisTemplate;
+    }
+}
